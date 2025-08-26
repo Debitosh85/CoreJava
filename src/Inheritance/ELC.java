e4c6be1a0991c5da4eb513203afa488ff29b6522
Debitosh85 <mohantydebitosh85@gmail.com>
2025-08-27 00:35:22 +0530

    Learned Access Modifiers: Established relationship between

---
+++ src/Inheritance/ELC.java
@@ -0,0 +1,15 @@
+package Inheritance;
+
+import oops.Test;
+
+public class ELC extends Test {
+
+    public static void main(String[] args) {
+
+        ELC e = new ELC();
+
+        System.out.println("The Value of x  from oops is :" + e.x);
+
+    }
+
+}
---
+++ src/oops/Test.java
@@ -0,0 +1,7 @@
+package oops;
+
+public class Test {
+
+    protected int x = 500;
+
+}
